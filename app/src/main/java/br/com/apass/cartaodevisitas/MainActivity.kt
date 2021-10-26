393eb54264c9585dadd3e44eb1c60b30449e6a85
Anderson Passos Silva <anders.ps02@gmail.com>
2021-10-25 22:25:44 -0300

    Adiciona ViewBinding

--- app/src/main/java/br/com/apass/cartaodevisitas/MainActivity.kt
+++ app/src/main/java/br/com/apass/cartaodevisitas/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.apass.cartaodevisitas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.apass.cartaodevisitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
